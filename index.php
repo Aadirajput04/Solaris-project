fb9df6efcccf11aeb35c65129f93ff0b08eb28a7
aaditya singh <aadityasingh396@gmail.com>
2023-07-10 15:23:26 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>home-solaris</title>
+</head>
+<body>
+   <?php
+    include "header.php";
+   ?>
+
+
+
+
+
+
+
+
+<?php
+    include "footer.php";
+   ?>
+
+<script src="script.js"></script>
+</body>
+</html>
